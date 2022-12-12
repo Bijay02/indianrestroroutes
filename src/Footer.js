665550c672e520067bba817b4a4f566426823b71
Bijay02 <bijaystha120@gmail.com>
2022-12-12 09:32:51 +0545

    second update

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import './footerstyle.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram
+} from "@fortawesome/free-brands-svg-icons";
 
-const Footer = () => {
+export default function SocialFlow() {
   return (
-    <div>Footer</div>
-  )
+    <div>
+      <p className="social-container">
+        <div>
+        <a
+          href="https://www.youtube.com/"
+          className="youtube social"
+        >
+          <FontAwesomeIcon icon={faYoutube} size="2x" />
+        </a>
+        <a
+          href="https://www.facebook.com/"
+          className="facebook social"
+        >
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+        <a
+          href="http://www.instagram.com/"
+          className="instagram social"
+        >
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </a>
+        <a href="https://wwww.twitter.com" className="twitter social">
+          <FontAwesomeIcon icon={faTwitter} size="2x" />
+        </a>
+        </div>
+        <div className="footer-text">&copy; Indianfoood.com</div>
+      </p>
+    </div>
+  );
 }
-
-export default Footer
